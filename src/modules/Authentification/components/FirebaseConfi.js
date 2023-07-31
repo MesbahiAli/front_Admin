802b1ba2d14dfa16d55172fb6579f90b8f942abf
alimesbahi <mali@adsolutions.ma>
2023-07-31 08:27:14 +0100

    Key FireBase in .Env

--- src/modules/Authentification/components/FirebaseConfi.js
+++ src/modules/Authentification/components/FirebaseConfi.js
@@ -2,13 +2,13 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 
 const firebaseConfi = {
-    apiKey: "AIzaSyBu9sPX2E7vFxcYrzRByC-UhqD3TwLEAXY",
-    authDomain: "testfirebase-5ea6a.firebaseapp.com",
-    projectId: "testfirebase-5ea6a",
-    storageBucket: "testfirebase-5ea6a.appspot.com",
-    messagingSenderId: "1079347390757",
-    appId: "1:1079347390757:web:2da7c35b25473630f4d2cc",
-    measurementId: "G-G2B5JNMJKB"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    appId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 firebase.initializeApp(firebaseConfi);
